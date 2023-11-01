b67bdbc3dca43b3a8a6fe2731ff57f45c9e9967d
Roni Costa <profronicosta@gmail.com>
2023-11-01 20:25:33 -0300

    Alteração no campo Estado

--- AgendaApp/models.py
+++ AgendaApp/models.py
@@ -12,6 +12,13 @@ class Contato(models.Model):
         ('V', 'Viúvo')
     ]
 
+    UFS = [
+        ('SP', 'São Paulo'),
+        ('MG', 'Minas Gerais'),
+        ('RJ', 'Rio de Janeiro'),
+        ('ES', 'Espírito Santo'),
+    ]
+
     nome = models.CharField(max_length=150)
     apelido = models.CharField(max_length=30)
     email = models.EmailField(max_length=100)
@@ -22,7 +29,7 @@ class Contato(models.Model):
     cep = models.CharField(max_length=9)
     bairro = models.CharField(max_length=150)
     cidade = models.CharField(max_length=100)
-    estado = models.CharField(max_length=50)
+    estado = models.CharField(max_length=2, choices=UFS)
     estado_civil = models.CharField(max_length=1, choices=ESTADO_CIVIS, null=True)
 
     def __str__(self):
